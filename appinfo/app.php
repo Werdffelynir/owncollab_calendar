eade0e9c46ba2cac710489264a88427712f83964
werdffelynir <werdffelynir@gmail.com>
2016-09-09 19:21:43 +0300

    sync

--- appinfo/app.php
+++ appinfo/app.php
@@ -13,10 +13,13 @@ namespace OCA\Owncollab_Calendar\AppInfo;
 
 use OCP\AppFramework\App;
 
-require_once __DIR__ . '/autoload.php';
+#require_once __DIR__ . '/autoload.php';
 
-$app = new App('owncollab_calendar');
-$container = $app->getContainer();
+
+if(\OCP\App::isEnabled('calendar')) {
+    $app = new App('owncollab_calendar');
+    $container = $app->getContainer();
+}
 
 /*
 $container->query('OCP\INavigationManager')->add(function () use ($container) {
